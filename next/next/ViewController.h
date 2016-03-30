c32807ed11237b84eed4fe50bba818f995b8fa59
Litao <389030378@qq.com>
2016-03-30 12:27:12 +0800

    gitHup

---
+++ next/next/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  next
+//
+//  Created by Ibokan on 16/3/30.
+//  Copyright © 2016年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ next/next/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  next
+//
+//  Created by Ibokan on 16/3/30.
+//  Copyright © 2016年 ibokan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"gitHup");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
